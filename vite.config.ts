6dc59895b3af842f10d4e3f573e99eaaafc77430
Kennedy <kennedy@MacBook-Pro.local>
2025-06-10 10:26:38 +0200

    Update config and dependencies for alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -8,7 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(), // <-- This plugin enables path alias support from tsconfig.json
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
